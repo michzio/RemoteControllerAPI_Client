22902a7c365aa3b6a2fb365581ca1d87c8bd0822
Michal Ziobro <michal.ziobro@uj.edu.pl>
2016-11-30 19:03:35 +0100

    tested on windows cygwin, some bugs fixed

--- test/test_transfer.c
+++ test/test_transfer.c
@@ -3,11 +3,8 @@
 //
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <memory.h>
-#include <CoreServices/CoreServices.h>
-#include <CoreGraphics/CoreGraphics.h>
 #include "test_transfer.h"
 #include "../networking/connection.h"
 #include "../client.h"
@@ -16,20 +13,13 @@
 #include "../../networking/transfer/binary_transfer.h"
 #include "../../unit_tests/test/assertion.h"
 #include "../../unit_tests/common/terminal.h"
-#include "../../comparers/comparer.h"
-#include "../../common/bitwise.h"
-#include "../../common/bitmaps.h"
-#include "../../common/libraries/lz4/lz4.h"
 
-#define TEST_PORT "3333"
 
-static void test_create_stream_conn(connection_handler_t test_service_handler) {
-
-    if(create_stream_conn(SERVER_ADDRESS, TEST_PORT, test_service_handler) != SUCCESS) {
-        fprintf(stderr, "create_stream_conn: failed!\n");
-        return;
-    }
-}
+#ifdef __APPLE__
+    #include <CoreServices/CoreServices.h>
+    #include <CoreGraphics/CoreGraphics.h>
+    #include "test_transfer_osx.c"
+#endif
 
 static result_t uint8_transfer_handler(sock_fd_t sock_fd) {
 
@@ -252,220 +242,23 @@ static void test_cstring_transfer(void) {
     test_create_stream_conn(cstring_transfer_handler);
 }
 
-static result_t png_transfer_handler(sock_fd_t sock_fd) {
-
-    result_t res = 0;
-    unsigned char *pngData = 0;
-    size_t pngDataLength = 0;
-    size_t width = 0, height = 0;
-
-    // get size of png image
-    recv_uint32(sock_fd, &width);
-    recv_uint32(sock_fd, &height);
-    // get length of png data
-    recv_uint32(sock_fd, &pngDataLength);
-
-    // get png data
-    pngData = (unsigned char *) malloc(sizeof(unsigned char)*pngDataLength);
-    res = recv_binary(sock_fd, PACKET_LENGTH, pngData, pngDataLength);
-
-    assert_equal_int(res, SUCCESS, "received PNG data from socket");
-    printf(ANSI_COLOR_CYAN "png image size (%zu, %zu)\n" ANSI_COLOR_RESET, width, height);
-
-    res = (result_t) fwrite_binaries("/Users/michzio/Desktop/test_client_screen_snapshot.png", pngData, pngDataLength);
-    assert_equal_int(res, SUCCESS, "PNG data saved to file");
-
-    free(pngData);
-
-    return SUCCESS;
-}
-
-static void test_png_transfer(void) {
-    test_create_stream_conn(png_transfer_handler);
-}
-
-static int count = 0;
-
-static result_t display_stream_png_transfer_handler(sock_fd_t sock_fd) {
-
-    result_t res = 0;
-    unsigned char *pngData = 0;
-    size_t pngDataLength = 0;
-    size_t width = 0, height = 0;
-
-    // get size of png frame
-    if(recv_uint32(sock_fd, &width) != SUCCESS) return FAILURE;
-    if(recv_uint32(sock_fd, &height) != SUCCESS) return FAILURE;
-    // get length of png data
-    if(recv_uint32(sock_fd, &pngDataLength) != SUCCESS) return FAILURE;
-
-    // get png data
-    pngData = (unsigned char *) malloc(sizeof(unsigned char)*pngDataLength);
-    res = recv_binary(sock_fd, PACKET_LENGTH, pngData, pngDataLength);
-    assert_equal_int(res, SUCCESS, "received PNG data from socket");
-
-    if(res != SUCCESS) return FAILURE;
-    printf(ANSI_COLOR_CYAN "png frame size (%zu, %zu)\n" ANSI_COLOR_RESET, width, height);
-
-    count++;
-    char filePath[256];
-    sprintf(filePath, "/Users/michzio/Desktop/display_stream/%d.png", count);
-    res = (result_t) fwrite_binaries(filePath, pngData, pngDataLength);
-    assert_equal_int(res, SUCCESS, "PNG data saved to file");
-
-    printf("Frame counter: %d\n", count);
-    free(pngData);
-
-    return SUCCESS;
-}
-
-static result_t display_stream_png_transfer_handler_loop(sock_fd_t sock_fd) {
-
-    while(1)
-        if(display_stream_png_transfer_handler(sock_fd) != SUCCESS) return FAILURE;
-}
-
-static void test_display_stream_png_transfer(void) {
-    count = 0;
-    test_create_stream_conn(display_stream_png_transfer_handler_loop);
-}
-
-#define FRAME_BUFFER_SIZE WIDTH_5K_16_9*HEIGHT_5K_16_9*RGBA_COMPONENTS_PER_PIXEL
-#define LZ4_BUFFER_SIZE LZ4_COMPRESSBOUND(FRAME_BUFFER_SIZE)
-static unsigned char xorFrameBuffer[FRAME_BUFFER_SIZE];
-static unsigned char frameBuffer[FRAME_BUFFER_SIZE] = { 0 };
-static size_t frameBufferLength = 0;
-static size_t frameWidth = 0, frameHeight = 0;
-static unsigned char lz4Data[LZ4_BUFFER_SIZE];
-
-static result_t display_stream_lz4_transfer_handler(sock_fd_t sock_fd) {
-
-    result_t res = 0;
-    size_t lz4DataLength = 0;
-
-    // get length of LZ4 compressed data
-    if (recv_uint32(sock_fd, &lz4DataLength) != SUCCESS) return FAILURE;
-    // get LZ4 compressed data
-    res = recv_binary(sock_fd, PACKET_LENGTH, lz4Data, lz4DataLength);
-    assert_equal_int(res, SUCCESS, "received LZ4 compressed data from socket");
-    if (res != SUCCESS) return FAILURE;
-
-    printf(ANSI_COLOR_CYAN "LZ4 data length: %zu\n" ANSI_COLOR_RESET, lz4DataLength);
-
-    // decompress frame data with LZ4
-    if(LZ4_decompress_safe(lz4Data, xorFrameBuffer, lz4DataLength, frameBufferLength) < 0) {
-        fprintf(stderr, "%s: Error at decompression of LZ4 data.\n", __func__);
-        return FAILURE;
-    }
-
-    bitwise_xor64_static(frameBuffer, xorFrameBuffer, frameBufferLength, frameBuffer);
-
-    CGContextRef context = CGBitmapContextCreate(frameBuffer, frameWidth, frameHeight, 8,
-                                                 COMPONENTS_PER_PIXEL * frameWidth, CGColorSpaceCreateDeviceRGB(), kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    CGImageRef image = CGBitmapContextCreateImage(context);
-    CFRelease(context);
-
-    count++;
-    printf("Frame counter: %d\n", count);
-    //char filePath[256];
-    //sprintf(filePath, "/Users/michzio/Desktop/display_stream/%d.bmp", count);
-
-    return SUCCESS;
-}
-
-static result_t display_stream_lz4_transfer_handler_loop(sock_fd_t sock_fd) {
-
-    if (recv_uint32(sock_fd, &frameWidth) != SUCCESS) return FAILURE;
-    if (recv_uint32(sock_fd, &frameHeight) != SUCCESS) return FAILURE;
-    printf(ANSI_COLOR_CYAN "frame size (%zu, %zu)\n" ANSI_COLOR_RESET, frameWidth, frameHeight);
-
-    frameBufferLength = frameWidth*frameHeight*RGBA_COMPONENTS_PER_PIXEL;
-
-    while(1)
-        if(display_stream_lz4_transfer_handler(sock_fd) != SUCCESS) return FAILURE;
-}
-
-static void test_display_stream_lz4_transfer(void) {
-    count = 0;
-    test_create_stream_conn(display_stream_lz4_transfer_handler_loop);
-}
-
-static unsigned char updateBuffer[FRAME_BUFFER_SIZE] = { 0 };
-static size_t updateBufferLength = 0;
-
-static result_t display_stream_updates_lz4_transfer_handler(sock_fd_t sock_fd) {
-
-    result_t res = 0;
-    size_t offsetX = 0, offsetY = 0;
-    size_t updateWidth = 0, updateHeight = 0;
-    size_t lz4DataLength = 0;
-
-    // recv update frame metadata
-    if (recv_uint32(sock_fd, &offsetX) != SUCCESS) return FAILURE;
-    if (recv_uint32(sock_fd, &offsetY) != SUCCESS) return FAILURE;
-    if (recv_uint32(sock_fd, &updateWidth) != SUCCESS) return FAILURE;
-    if (recv_uint32(sock_fd, &updateHeight) != SUCCESS) return FAILURE;
-
-    // get length of LZ4 compressed data
-    if (recv_uint32(sock_fd, &lz4DataLength) != SUCCESS) return FAILURE;
-    // get LZ4 compressed data
-    res = recv_binary(sock_fd, PACKET_LENGTH, lz4Data, lz4DataLength);
-    assert_equal_int(res, SUCCESS, "received LZ4 compressed data from socket");
-    if (res != SUCCESS) return FAILURE;
-
-    printf(ANSI_COLOR_CYAN "LZ4 data length: %zu\n" ANSI_COLOR_RESET, lz4DataLength);
-
-    // decompress update data with LZ4
-    updateBufferLength = updateWidth*updateHeight*RGBA_COMPONENTS_PER_PIXEL;
-    if(LZ4_decompress_safe(lz4Data, updateBuffer, lz4DataLength, updateBufferLength) < 0) {
-        fprintf(stderr, "%s: Error at decompression of LZ4 data.\n", __func__);
-        return FAILURE;
-    }
-
-    // update frame buffer with new data
-    size_t updateOffset = offsetY*frameWidth*RGBA_COMPONENTS_PER_PIXEL + offsetX*RGBA_COMPONENTS_PER_PIXEL;
-    memcpy(frameBuffer + updateOffset, updateBuffer, updateBufferLength);
-
-    count++;
-    printf("Frame counter: %d\n", count);
-
-    return SUCCESS;
-}
-
-static result_t display_stream_updates_lz4_transfer_handler_loop(sock_fd_t sock_fd) {
-
-    if (recv_uint32(sock_fd, &frameWidth) != SUCCESS) return FAILURE;
-    if (recv_uint32(sock_fd, &frameHeight) != SUCCESS) return FAILURE;
-    printf(ANSI_COLOR_CYAN "frame size (%zu, %zu)\n" ANSI_COLOR_RESET, frameWidth, frameHeight);
-
-    frameBufferLength = frameWidth*frameHeight*RGBA_COMPONENTS_PER_PIXEL;
-
-    while(1)
-        if(display_stream_updates_lz4_transfer_handler(sock_fd) != SUCCESS) return FAILURE;
-}
-
-static void test_display_stream_updates_lz4_transfer(void) {
-    count = 0;
-    test_create_stream_conn(display_stream_updates_lz4_transfer_handler_loop);
-}
-
 static void run_tests(void) {
 
     printf(ANSI_COLOR_BLUE "Integration Test - requires to run: 'rc_server' program only with 'test_server_transfer.run_tests()' \n" ANSI_COLOR_RESET);
-    /*test_uint8_transfer();
-    test_uint16_transfer();
-    test_uint32_transfer();
-    test_uint64_transfer();
-    test_int8_transfer();
-    test_int16_transfer();
-    test_int32_transfer();
-    test_int64_transfer();
-    test_binary_transfer();
-    test_cstring_transfer();
-    test_png_transfer(); */
+    //test_uint8_transfer();
+    //test_uint16_transfer();
+    //test_uint32_transfer();
+    //test_uint64_transfer();
+    //test_int8_transfer();
+    //test_int16_transfer();
+    //test_int32_transfer();
+    //test_int64_transfer();
+    //test_binary_transfer();
+    //test_cstring_transfer();
+    //test_png_transfer();
     //test_display_stream_png_transfer();
     //test_display_stream_lz4_transfer();
-    test_display_stream_updates_lz4_transfer();
+    //test_display_stream_updates_lz4_transfer();
 }
 
 test_client_transfer_t test_client_transfer = { .run_tests = run_tests };
--- test/test_transfer.h
+++ test/test_transfer.h
@@ -5,10 +5,23 @@
 #ifndef REMOTECONTROLLERAPI_CLIENT_TEST_TRANSFER_H
 #define REMOTECONTROLLERAPI_CLIENT_TEST_TRANSFER_H
 
+#include "../networking/connection.h"
+#include "../client.h"
+
 typedef struct {
     void (*run_tests)(void);
 } test_client_transfer_t;
 
 extern test_client_transfer_t test_client_transfer;
+
+#define TEST_PORT "3333"
+
+static void test_create_stream_conn(connection_handler_t test_service_handler) {
+
+    if(create_stream_conn(SERVER_ADDRESS, TEST_PORT, test_service_handler) != SUCCESS) {
+        fprintf(stderr, "create_stream_conn: failed!\n");
+        return;
+    }
+}
 
 #endif //REMOTECONTROLLERAPI_CLIENT_TEST_TRANSFER_H
