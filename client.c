ae0e32fafc123e4ed09b45b6eb5d7aadc03608fa
michzio <michzio@hotmail.com>
2017-05-22 15:53:30 +0200

    end_client() function implemented

--- client.c
+++ client.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <string.h>
+#include <errno.h>
 #include "client.h"
 #include "networking/stream_client.h"
 #include "networking/datagram_client.h"
@@ -18,6 +20,13 @@ result_t start_client(client_t client, client_info_t *client_info) {
 }
 
 result_t end_client(client_info_t *client_info) {
+
+    sock_fd_t cs_fd = client_info_sock(client_info);
+
+    if(close(cs_fd) < 0) {
+        fprintf(stderr, "close: %s\n", strerror(errno));
+        return FAILURE;
+    }
 
     return SUCCESS;
 }
